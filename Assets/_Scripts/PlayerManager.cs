d1f6fa84519f12b642d1f73f44219f0ef5c5fa1d
pbbrausch <pbbrausch@gmail.com>
2023-03-04 11:18:41 -0500

    changes

--- Assets/_Scripts/PlayerManager.cs
+++ Assets/_Scripts/PlayerManager.cs
@@ -67,6 +67,8 @@ public class PlayerManager : NetworkBehaviour
     {
         if (isOwned)
         {
+            GameManager.instance.DestroyPlayerListItems();
+
             if (isServer)
             {
                 Manager.StopHost();
@@ -80,8 +82,6 @@ public class PlayerManager : NetworkBehaviour
 
     private void OnDisable()
     {
-        GameManager.instance.DestroyPlayerListItems();
-
         SteamMatchmaking.LeaveLobby((CSteamID)LobbyManager.instance.joinedLobbyID);
     }
 
