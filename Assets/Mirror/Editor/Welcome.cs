a7183d780cf7c62162833254b167201b1c799b69
pbbrausch <114188523+pbbrausch@users.noreply.github.com>
2023-03-04 13:25:43 -0500

    changes

--- Assets/Mirror/Editor/Welcome.cs
+++ Assets/Mirror/Editor/Welcome.cs
@@ -15,7 +15,7 @@ namespace Mirror
             if (!SessionState.GetBool("MIRROR_WELCOME", false))
             {
                 SessionState.SetBool("MIRROR_WELCOME", true);
-                Debug.Log("Mirror | mirror-networking.com | discord.gg/N9QVxbM");
+                //Debug.Log("Mirror | mirror-networking.com | discord.gg/N9QVxbM");
             }
         }
     }
